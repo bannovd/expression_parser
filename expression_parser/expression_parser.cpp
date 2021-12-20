59614984b3976c625fe736d5328e562d9a27a6b3
Дмитрий Баннов <bannovd@yandex.ru>
2021-12-20 06:16:48 +0400

    init commit

---
+++ expression_parser/expression_parser.cpp
@@ -0,0 +1,12 @@
+﻿// expression_parser.cpp: определяет точку входа для приложения.
+//
+
+#include "expression_parser.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello world!" << endl;
+	return 0;
+}
